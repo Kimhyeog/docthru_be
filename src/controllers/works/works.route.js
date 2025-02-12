79c8054b9d3b5de9aa3a1038f5b072c339028161
sungmin <usm0259@kaist.ac.kr>
2025-02-13 01:18:55 +0900

    chore: make folder

---
+++ src/controllers/works/works.route.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const workRouter = express.Router();
+
+workRouter.get("/");
+
+module.exports = workRouter;
---
+++ src/services/work.service.js
@@ -0,0 +1,2 @@
+const workService = {};
+module.exports = workService;
