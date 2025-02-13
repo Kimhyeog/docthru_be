b8d4c47cd62008662b4216da6dd757cfdf2e078f
sungmin <usm0259@kaist.ac.kr>
2025-02-13 14:31:06 +0900

    fix:participateChallenge , feat: add uadapte work and deletework

--- src/controllers/works/works.route.js
+++ src/controllers/works/works.route.js
@@ -4,8 +4,18 @@ const { validateCreateWork } = require("../../validate/work.validate");
 
 const workRouter = express.Router();
 
-workRouter.get("/");
-// 챌린지 생성성
+// 작업물 조회 좋아요 순으로
+workRouter.get("/:challengeId/many", workService.getWorks);
+// 특정 작업물 조회
+workRouter.get("/:workId", workService.getWork);
+
+// 작업물 생성
 workRouter.post("/:challengeId", validateCreateWork, workService.createWork);
+
+// 작업물 수정
+workRouter.put("/:workId", validateCreateWork, workService.updateWork);
+
+// 작업물 삭제
+workRouter.delete("/:challengeId", workService.deleteWork);
 
 module.exports = workRouter;
--- src/services/work.service.js
+++ src/services/work.service.js
@@ -1,6 +1,26 @@
 const prisma = require("../db/prisma/client");
 const { asyncHandler } = require("../middlewares/error.middleware");
 
+const getWorks = asyncHandler(async (req, res, next) => {
+  const { cursor } = req.query;
+  const challengeId = req.params.challengeId;
+  const works = await prisma.work.findMany({
+    where: { challengeId },
+    take: 5,
+    orderBy: { likeCount: "desc" },
+    cursor: cursor ? { id: cursor } : undefined,
+  });
+  const nextCursor = works.length === 5 ? works[works.length - 1].id : null;
+  res.status(200).send({ works, nextCursor });
+});
+
+const getWork = asyncHandler(async (req, res, next) => {
+  const workId = req.params.workId;
+  const work = await prisma.work.findUnique({ where: { id: workId } });
+  if (!work) throw new Error("404/work not found");
+  res.status(200).send(work);
+});
+
 const createWork = asyncHandler(async (req, res, next) => {
   const challengeId = req.params.challengeId;
   const userId = req.userId;
@@ -17,8 +37,8 @@ const createWork = asyncHandler(async (req, res, next) => {
   if (!participate)
     throw new Error("400/This is a challenge you have not participated in");
 
-  const existingWork = await prisma.work.findUnique({
-    where: { userId_challengeId: { userId, challengeId } },
+  const existingWork = await prisma.work.findFirst({
+    where: { userId, challengeId, isSubmitted: true },
   });
   if (existingWork) throw new Error("400/you already create work");
   //조건이 만족 되면 만드는데 바디에서 번역문을 받아야함
@@ -35,5 +55,50 @@ const createWork = asyncHandler(async (req, res, next) => {
   res.status(200).send(work);
 });
 
-const workService = { createWork };
+// 수정 및 삭제에서 완료된 챌린지인지 확인하는 것도 필요함
+
+const updateWork = asyncHandler(async (req, res, next) => {
+  const workId = req.params.workId;
+  const userId = req.userId;
+  const { description } = req.body;
+  // work 찾고 유저 아이디 확인
+  let work = await prisma.work.findUnique({ where: { id: workId } });
+  if (!work) throw new Error("404/work not found");
+  if (work.userId !== userId) throw new Error("401/unathorization");
+
+  //조건이 만족 되면 만드는데 바디에서 번역문을 받아야함
+  work = await prisma.work.update({
+    where: { id: workId },
+    data: {
+      description,
+      lastModifiedAt: new Date(),
+    },
+  });
+  res.status(200).send(work);
+});
+
+const deleteWork = asyncHandler(async (req, res, next) => {
+  //
+  await prisma.$transaction(async (prisma) => {
+    const challengeId = req.params.challengeId;
+    const userId = req.userId;
+    const work = await prisma.work.findFirst({
+      where: { challengeId, userId, isSubmitted: true },
+    });
+    await prisma.participate.delete({
+      where: { userId_challengeId: { userId, challengeId } },
+    });
+    await prisma.challenge.update({
+      where: { id: challengeId },
+      data: { participants: { decrement: 1 } },
+    });
+
+    if (work.userId !== userId) throw new Error("401/unathorization");
+    await prisma.work.deleteMany({ where: { userId, challengeId } });
+  });
+
+  res.sendStatus(204);
+});
+
+const workService = { getWorks, getWork, createWork, updateWork, deleteWork };
 module.exports = workService;
